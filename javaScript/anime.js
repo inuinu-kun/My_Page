9524192e1ac802c77f7fad8f3c22108a30ff9464
LAPTOP-NJL5SK39\kitin <inukunkun50@gmail.com>
2024-09-11 16:36:35 +0900

    自作サイト完成 mp4ファイルは容量が大きいので除外

---
+++ javaScript/anime.js
@@ -0,0 +1,12 @@
+document.body.animate(
+    {
+      // 背景色を変化させる
+      background: ["#FFF","#0820f8", "linear-gradient(#c3c7ed,#9eebed)"],
+    },
+    {
+      // 終了時の状態で止める
+      fill: "forwards",
+      // 1000ミリ秒（＝1秒）かけてアニメーション
+      duration: 3000
+    }
+  );
